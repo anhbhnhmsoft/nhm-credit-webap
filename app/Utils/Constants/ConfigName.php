115852fc6c9191bdef4d3334bf7b767469dc4db8
maiduong <duongmdph40323@gmail.com>
2025-10-09 10:20:13 +0700

    Duongmd - 18 - Manager Configs

--- app/Utils/Constants/ConfigName.php
+++ app/Utils/Constants/ConfigName.php
@@ -4,4 +4,8 @@ namespace App\Utils\Constants;
 
 enum ConfigName: string
 {
+    case LOGO = 'LOGO';
+    case ADMIN_ACCOUNT_BANK_NAME = 'ADMIN_ACCOUNT_BANK_NAME';
+    case ADMIN_ACCOUNT_BANK_ACCOUNT = 'ADMIN_ACCOUNT_BANK_ACCOUNT';
+    case ADMIN_ACCOUNT_BANK_BIN = 'ADMIN_ACCOUNT_BANK_BIN';
 }
